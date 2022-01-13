c08f8a6a0c22ac30aac732244d0ef8cc32c51a05
Erik Svensson <eosvensson@gmail.com>
2022-01-13 08:59:57 +0100

    Setup complete

---
+++ src/main/java/com/example/g39restworkshop/G39RestWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.g39restworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G39RestWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G39RestWorkshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/g39restworkshop/G39RestWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.g39restworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G39RestWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
