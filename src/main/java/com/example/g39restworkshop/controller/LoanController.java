31d7a4593e17ad1939af88b59606e38414c450ff
Erik Svensson <eosvensson@gmail.com>
2022-01-14 13:57:45 +0100

    Added LoanController. Finished non optionals of part 4

---
+++ src/main/java/com/example/g39restworkshop/controller/LoanController.java
@@ -0,0 +1,59 @@
+package com.example.g39restworkshop.controller;
+
+import com.example.g39restworkshop.model.dto.LoanDTO;
+import com.example.g39restworkshop.service.interfaces.LoanService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class LoanController {
+
+    private final LoanService loanService;
+
+    @GetMapping("/api/v1/loans")
+    public ResponseEntity<List<LoanDTO>> find(@RequestParam(name = "search", defaultValue = "all") String search,
+                                              @RequestParam(name = "value", defaultValue = "") String value){
+        List<LoanDTO> result;
+        switch (search.toLowerCase()){
+            case "all":
+                result = loanService.findAll();
+                break;
+            case "user":
+                result = loanService.findByUserId(Integer.valueOf(value));
+                break;
+            case "book":
+                result = loanService.findByBookId(Integer.valueOf(value));
+                break;
+            case "concluded":
+                result = loanService.findByConcluded(Boolean.parseBoolean(value));
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid search type: '" + search + "'. Valid types are 'all', 'user', 'book' and 'concluded'.");
+        }
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("/api/v1/loans")
+    public ResponseEntity<LoanDTO> create(@RequestBody LoanDTO dto){
+        return ResponseEntity.status(201).body(loanService.create(dto));
+    }
+
+    @GetMapping("/api/v1/loans/{id}")
+    public ResponseEntity<LoanDTO> findById(@PathVariable("id") Integer id){
+        return ResponseEntity.ok(loanService.findById(id));
+    }
+
+    @PutMapping("/api/v1/loans/{id}")
+    public ResponseEntity<LoanDTO> update(@PathVariable("id") Integer id, @RequestBody LoanDTO dto){
+        return ResponseEntity.ok(loanService.update(id, dto));
+    }
+
+    @DeleteMapping("/api/v1/loans/{id}")
+    public ResponseEntity<Boolean> delete(@PathVariable("id") Integer id){
+        return ResponseEntity.ok(loanService.delete(id));
+    }
+}
